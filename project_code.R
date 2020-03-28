6d4be847a48b470fdeb826298eceabf9523be36d
Rafal-Nowicki <rn68802@student.sgh.waw.pl>
2020-03-28 23:22:41 +0100

    next post

--- project_code.R
+++ project_code.R
@@ -21,6 +21,14 @@ search_eurostat("unemployment") %>%
 tertiary <- search_eurostat("tertiary") %>%
   arrange(desc(`last update of data`))
 
+idx_t <- which(grepl("NUTS2", tertiary$title))
+
+tertiary[idx_t,] %>%
+  View()
+
+tertiary_ratio <- get_eurostat("educ_uoe_enrt06") %>%
+  subset(grepl("DE([0-9]|[A-Z]){2}", geo))
+
 
 gdp <- get_eurostat("nama_10r_2gdp") %>%
   subset(grepl("DE([0-9]|[A-Z]){2}", geo)) %>%
@@ -35,7 +43,7 @@ unemp <- get_eurostat("lfst_r_lfu3rt") %>%
 hh_inc <- get_eurostat("nama_10r_2hhinc") %>%
   subset(grepl("DE([0-9]|[A-Z]){2}", geo)) %>%
   filter(time == "2017-01-01", unit == "EUR_HAB", na_item == "B5N") %>%
-  transmute(geo, houshold_income = values)
+  transmute(geo, household_income = values)
 
 gva_grwth <- get_eurostat("nama_10r_2gvagr") %>%
   subset(grepl("DE([0-9]|[A-Z]){2}", geo)) %>%
@@ -56,6 +64,27 @@ map <- readOGR(".", "NUTS_RG_10M_2016_4326_LEVL_2") %>%
 map <- map[map@data$CNTR_CODE %in% "DE", ]
 
 plot(map)
+
+# linear model for Germany #
+
+model_germany <- lm(data = df, household_income ~ unemployment + gva_growth)
+summary(model_germany)
+
+ger_res <- model_germany$residuals
+
+df_res <- cbind(df, ger_res)
+map$NUTS_ID
+
+spatial_data <- merge(y = df_res, x = map, by.y = "geo", by.x = "NUTS_ID")
+
+green_area <- rgb(24, 121, 104, 80, names = NULL, maxColorValue = 255)
+pal <- colorRampPalette(c("white", green_area), bias = 1)
+pal <- colorRampPalette(c("red", "green"))
+
+spplot(spatial_data, zcol = "ger_res", colorkey = TRUE, col.regions = pal(100), cuts = 99,
+       par.settings = list(axis.line = list(col =  'transparent')),
+       main = "residuals from regression")
+
 
 #### BDL API ####
 search_variables("bezrobocia") %>%
